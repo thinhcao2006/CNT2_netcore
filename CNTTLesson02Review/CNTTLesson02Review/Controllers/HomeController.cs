4631a6065410306000707ab27a6a9b2c7b323b64
thinhcao2006 <thinhcao500@gmail.com>
2026-08-27 20:03:07 +0700

    CNTTLesson02

---
+++ CNTTLesson02Review/CNTTLesson02Review/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using CNTTLesson02Review.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CNTTLesson02Review.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CNTTLesson02Review/CNTTLesson02Review/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CNTTLesson02Review.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
